e30830da21ccfd85a24d9a053e8065a85fa216ac
Emmanuel Pilliat <epilliat@gmail.com>
2026-03-03 22:36:30 +0100

    fixing device for subarrays

--- ext/CUDA/device.jl
+++ ext/CUDA/device.jl
@@ -1,7 +1,10 @@
-import KernelIntrinsics: select_device!, get_device, get_warpsize, list_devices
+import KernelIntrinsics: select_device!, get_device, get_warpsize, list_devices, device, name
 
 
 select_device!(::CUDABackend, i::Integer) = CUDA.device!(i - 1)
 get_device(::CUDABackend) = CUDA.device()
 get_warpsize(::CUDABackend) = 32
 list_devices(::CUDABackend) = collect(CUDA.devices())
+name(dev::CUDA.CuDevice) = CUDA.name(dev)
+device(src::CUDA.CuArray) = CUDA.device(src)
+device() = CUDA.device()
--- src/device.jl
+++ src/device.jl
@@ -2,3 +2,9 @@ function select_device! end
 function get_device end
 function warpsize end
 function list_devices end
+function device end
+function name end
+
+
+
+device(x::SubArray) = device(parent(x))
